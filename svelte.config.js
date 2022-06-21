9d7b3182f6db9edac72133b1f2945ac0ff5694d3
Alex <alexandre.olemans@gmail.com>
2022-06-22 00:09:27 +0200

    format

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,6 @@ import preprocessor from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-
 	preprocess: preprocessor({
 		scss: {
 			prependData: "@import './src/global.scss';"
