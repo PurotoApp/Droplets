39af83fddc9114df0ad3800abb8cdca6e861143e
Alex <alexandre.olemans@gmail.com>
2022-06-21 23:21:55 +0200

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,18 @@
+import adapter from '@sveltejs/adapter-auto';
+import preprocessor from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+
+	preprocess: preprocessor({
+		scss: {
+			prependData: "@import './src/global.scss';"
+		}
+	}),
+
+	kit: {
+		adapter: adapter()
+	}
+};
+
+export default config;
