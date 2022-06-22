d71d39765a78192a12ffa8aa015fed9604a2eecf
Alex <alexandre.olemans@gmail.com>
2022-06-22 18:05:34 +0200

    Add a load screen

---
+++ src/components/Loading.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Icon from './Icon.svelte';
+</script>
+
+<div class="bg">
+	<div class="icon">
+		<Icon size="6rem" stroke="purple" />
+	</div>
+</div>
+<div class="invisible">
+	<slot />
+</div>
+
+<style lang="scss">
+	.bg {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: $primary;
+		z-index: -1;
+
+		.icon {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			animation: pulse 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+		}
+	}
+
+	.invisible {
+		opacity: 0;
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,18 @@
-<slot />
+<script>
+	import { onMount } from 'svelte';
+	import Loading from '../components/Loading.svelte';
+
+	let isLoading = true;
+
+	onMount(() => {
+		isLoading = false;
+	});
+</script>
+
+{#if isLoading}
+	<Loading />
+{:else}
+	<slot />
+{/if}
 
 <style lang="scss"></style>
