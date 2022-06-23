00fb1722423cb0900763c99967d5dcaa5bfeed2a
sigmanificient <edhyjox@gmail.com>
2022-06-23 23:36:53 +0200

    :art: Reformat code

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,27 +1,27 @@
 <script context="module">
-	export function load({ error, status }) {
-		return {
-			props: {
-				status: status,
-				message: error.message
-			}
-		};
-	}
+  export function load({error, status}) {
+    return {
+      props: {
+        status: status,
+        message: error.message
+      }
+    };
+  }
 </script>
 
 <script>
-	export let status;
-	export let message;
+  export let status;
+  export let message;
 </script>
 
 <div class="errorHandler">
-	<h1>{status}</h1>
-	<p>{message}</p>
+  <h1>{status}</h1>
+  <p>{message}</p>
 </div>
 
 <style lang="scss">
-	.errorHandler {
-		text-align: center;
-		margin-top: 2rem;
-	}
+  .errorHandler {
+    text-align: center;
+    margin-top: 2rem;
+  }
 </style>
--- src/routes/index@reset.svelte
+++ src/routes/index@reset.svelte
@@ -1,289 +1,290 @@
 <script>
-	import Icon from '../components/icons/Icon.svelte';
-	import Twitter from '../components/icons/Twitter.svelte';
-	import Discord from '../components/icons/Discord.svelte';
+  import Icon from '../components/icons/Icon.svelte';
+  import Twitter from '../components/icons/Twitter.svelte';
+  import Discord from '../components/icons/Discord.svelte';
 
-	let menuOpen = true;
+  let menuOpen = true;
 
-	function toggleMenu() {
-		menuOpen = !menuOpen;
-	}
+  function toggleMenu() {
+    menuOpen = !menuOpen;
+  }
 </script>
 
 <svelte:head>
-	<title>Puroto - We are building the fluffiest social network</title>
-	<meta name="description" content="We are building the fluffiest social network" />
+  <title>Puroto - We are building the fluffiest social network</title>
+  <meta name="description" content="We are building the fluffiest social network" />
 </svelte:head>
 
 <div class="landing">
-	<header class="head">
-		<a class="logo" href="/">
-			<Icon size="1.75rem" stroke="#878CFB" />
-			<span>Puroto</span>
-		</a>
-		<div class="hamburger">
-			<svg on:click={toggleMenu}
-				class="icon"
-				width="24"
-				height="24"
-				viewBox="0 0 32 27"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<g fill="black">
-				<path style="fill:#FFF;" d="M0 0H32V3H0V0Z" />
-				<path style="fill:#FFF;" d="M0 12H32V15H0V12Z" />
-				<path style="fill:#FFF;" d="M0 24H32V27H0V24Z" />
-				</g>
-			</svg>
-			{#if menuOpen}
-			<div class="container">
-				<div class="menu">
-					<a class="patreon" href="/#">Support Us</a> <!-- THIS NEEDS FIXES, IT DOESN'T WORK AS INTENDED -->
-					<a class="join" href="/#">Apply</a>
-				</div>
-			</div>
-			{/if}
-		</div>
-		<div class="grouplink">
-			<a class="patreon" rel="noopener" target="_blank" href="/#">
-				Support Us
-			</a>
-			<a class="join" href="/#">
-				Apply
-			</a>
-		</div>
-	</header>
-	<div class="body">
-		<div class="intro">
-			<div class="intro-text">
-				<h1>We are building the fluffiest social network.</h1>
-			</div>
-			<div class="ext-links">
-				<a class="link github" rel="noopener" target="_blank" href="/#">
-					<div class="desk">
-						<img src="/Github.png" style="width:2rem;height:2rem;" alt="Github" />
-					</div>
-					<div class="mobile">
-						<img src="/Github.png" style="width:1.5rem;height:1.5rem;" alt="Github" />
-					</div>
-					<span class="github">Github</span>
-				</a>
-				<a class="link twitter" rel="noopener" target="_blank" href="/#">
-					<div class="desk">
-						<Twitter size="2rem" />
-					</div>
-					<div class="mobile">
-						<Twitter size="1.5rem" />
-					</div>
-					<span class="twitter">Twitter</span>
-				</a>
-				<a class="link discord" rel="noopener" target="_blank" href="/#">
-					<div class="desk">
-						<Discord size="2rem" />
-					</div>
-					<div class="mobile">
-						<Discord size="1.5rem" />
-					</div>
-					<span class="discord">Discord</span>
-				</a>
-			</div>
-		</div>
-	</div>
+  <header class="head">
+    <a class="logo" href="/">
+      <Icon size="1.75rem" stroke="#878CFB" />
+      <span>Puroto</span>
+    </a>
+    <div class="hamburger">
+      <svg on:click={toggleMenu}
+           class="icon"
+           width="24"
+           height="24"
+           viewBox="0 0 32 27"
+           fill="none"
+           xmlns="http://www.w3.org/2000/svg"
+      >
+        <g fill="black">
+          <path style="fill:#FFF;" d="M0 0H32V3H0V0Z" />
+          <path style="fill:#FFF;" d="M0 12H32V15H0V12Z" />
+          <path style="fill:#FFF;" d="M0 24H32V27H0V24Z" />
+        </g>
+      </svg>
+      {#if menuOpen}
+        <div class="container">
+          <div class="menu">
+            <a class="patreon" href="/#">Support Us</a> <!-- THIS NEEDS FIXES, IT DOESN'T WORK AS INTENDED -->
+            <a class="join" href="/#">Apply</a>
+          </div>
+        </div>
+      {/if}
+    </div>
+    <div class="grouplink">
+      <a class="patreon" rel="noopener" target="_blank" href="/#">
+        Support Us
+      </a>
+      <a class="join" href="/#">
+        Apply
+      </a>
+    </div>
+  </header>
+  <div class="body">
+    <div class="intro">
+      <div class="intro-text">
+        <h1>We are building the fluffiest social network.</h1>
+      </div>
+      <div class="ext-links">
+        <a class="link github" rel="noopener" target="_blank" href="/#">
+          <div class="desk">
+            <img src="/Github.png" style="width:2rem;height:2rem;" alt="Github" />
+          </div>
+          <div class="mobile">
+            <img src="/Github.png" style="width:1.5rem;height:1.5rem;" alt="Github" />
+          </div>
+          <span class="github">Github</span>
+        </a>
+        <a class="link twitter" rel="noopener" target="_blank" href="/#">
+          <div class="desk">
+            <Twitter size="2rem" />
+          </div>
+          <div class="mobile">
+            <Twitter size="1.5rem" />
+          </div>
+          <span class="twitter">Twitter</span>
+        </a>
+        <a class="link discord" rel="noopener" target="_blank" href="/#">
+          <div class="desk">
+            <Discord size="2rem" />
+          </div>
+          <div class="mobile">
+            <Discord size="1.5rem" />
+          </div>
+          <span class="discord">Discord</span>
+        </a>
+      </div>
+    </div>
+  </div>
 </div>
 
 <style lang="scss">
-	.landing {
-		margin: 1% 5% 0 5%;
-	}
-	
-	.head {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		padding: 0.5rem;
-		
-		.logo {
-			display: flex;
-			align-items: center;
-			text-decoration: none;
-			user-select: none;
-			padding: 0.5rem;
+  .landing {
+    margin: 1% 5% 0 5%;
+  }
 
-			:global(svg) {
-				margin-right: 1rem;
-			}
+  .head {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem;
 
-			:global(span) {
-				color: #878CFB;
-				font-weight: 600; // semibold
-				font-size: 1.75rem;
-			}
-		}
+    .logo {
+      display: flex;
+      align-items: center;
+      text-decoration: none;
+      user-select: none;
+      padding: 0.5rem;
 
-		.hamburger {
-			cursor: pointer;
-			padding: 0.5rem;
-			user-select: none;
-			
-			.container {
-				position: absolute;
-				transform: translateX(-75%);
-				background-color: $secondary;
-				border-radius: 0.5rem;
-				padding: 0.5rem;
-				width: 50%;
-				
-				.patreon {
-					margin-right: 5rem;
-					color: #FF5A96;
-					font-weight: 600; // semibold
-					font-size: 1rem;
-					text-decoration: none;
-				}
+      :global(svg) {
+        margin-right: 1rem;
+      }
 
-				.join {
-					color: #878CFB;
-					font-weight: 600; // semibold
-					font-size: 1rem;
-					text-decoration: none;
-				}
-			}
-		}
+      :global(span) {
+        color: #878CFB;
+        font-weight: 600; // semibold
+        font-size: 1.75rem;
+      }
+    }
 
-		.grouplink {
-			display: none;
-		}
-	}
+    .hamburger {
+      cursor: pointer;
+      padding: 0.5rem;
+      user-select: none;
 
-	.body {
-		display: flex;
-		align-items: center;
-		margin-top: 1rem;
-		padding: -0.5rem;
+      .container {
+        position: absolute;
+        transform: translateX(-75%);
+        background-color: $secondary;
+        border-radius: 0.5rem;
+        padding: 0.5rem;
+        width: 50%;
 
-		.intro-text {
-			width: 70%;
-			font-size: 1rem;
-			color: #8799FB;
-		}
+        .patreon {
+          margin-right: 5rem;
+          color: #FF5A96;
+          font-weight: 600; // semibold
+          font-size: 1rem;
+          text-decoration: none;
+        }
 
-		.ext-links {
-			display: flex;
-			justify-content: space-between;
-			width: 60%;
-			margin-top: 4rem;
-			color: #9CABDF;
+        .join {
+          color: #878CFB;
+          font-weight: 600; // semibold
+          font-size: 1rem;
+          text-decoration: none;
+        }
+      }
+    }
 
-			.link {
-				color: #9CABDF;
-				font-weight: 600; // semibold
-				font-size: 1rem;
-				display: flex;
-				margin-right: 1.5rem;
-				user-select: none;
-				text-decoration:none;
+    .grouplink {
+      display: none;
+    }
+  }
 
-				.desk {
-					display: none;
-				}
+  .body {
+    display: flex;
+    align-items: center;
+    margin-top: 1rem;
+    padding: -0.5rem;
 
-				:global(span) {
-					margin-left: 0.5rem;
-				}
+    .intro-text {
+      width: 70%;
+      font-size: 1rem;
+      color: #8799FB;
+    }
+
+    .ext-links {
+      display: flex;
+      justify-content: space-between;
+      width: 60%;
+      margin-top: 4rem;
+      color: #9CABDF;
+
+      .link {
+        color: #9CABDF;
+        font-weight: 600; // semibold
+        font-size: 1rem;
+        display: flex;
+        margin-right: 1.5rem;
+        user-select: none;
+        text-decoration: none;
+
+        .desk {
+          display: none;
+        }
+
+        :global(span) {
+          margin-left: 0.5rem;
+        }
 
 
+        .github {
+          color: #FFF;
+        }
 
-				.github {
-					color: #FFF;
-				}
+        .twitter {
+          color: #1D9BF0;
+        }
 
-				.twitter {
-					color: #1D9BF0;
-				}
+        .discord {
+          color: #5865F2;
+        }
+      }
+    }
+  }
 
-				.discord {
-					color: #5865F2;
-				}
-			}
-		}
-	}
+  @media (min-width: 768px) {
+    .landing {
+      margin: 1% 10% 0 10%;
+    }
 
-	@media (min-width: 768px) {
-		.landing {
-			margin: 1% 10% 0 10%;
-		}
-	
-		.head {
-			padding: 0.5rem;
+    .head {
+      padding: 0.5rem;
 
-			.logo {
-				:global(svg) {
-					margin-right: 1rem;
-				}
+      .logo {
+        :global(svg) {
+          margin-right: 1rem;
+        }
 
-				:global(span) {
-					font-weight: 600; // semibold
-					font-size: 1.75rem;
-				}
-			}
+        :global(span) {
+          font-weight: 600; // semibold
+          font-size: 1.75rem;
+        }
+      }
 
-			.hamburger {
-				display: none;
-			}
+      .hamburger {
+        display: none;
+      }
 
-			.grouplink {
-				display: flex;
-				user-select: none;
-				.patreon {
-					margin-right: 5rem;
-					color: #FF5A96;
-					font-weight: 600; // semibold
-					font-size: 1.5rem;
-					text-decoration: none;
-					padding: 1rem;
-				}
+      .grouplink {
+        display: flex;
+        user-select: none;
 
-				.join {
-					color: #878CFB;
-					font-weight: 600; // semibold
-					font-size: 1.5rem;
-					text-decoration: none;
-					padding: 1rem;
-				}
-			}
-		}
+        .patreon {
+          margin-right: 5rem;
+          color: #FF5A96;
+          font-weight: 600; // semibold
+          font-size: 1.5rem;
+          text-decoration: none;
+          padding: 1rem;
+        }
 
-		.body {
-			margin-top: 7rem;
-			padding: -0.5rem;
+        .join {
+          color: #878CFB;
+          font-weight: 600; // semibold
+          font-size: 1.5rem;
+          text-decoration: none;
+          padding: 1rem;
+        }
+      }
+    }
 
-			.intro-text {
-				width: 70%;
-				font-size: 1.5rem;
-			}
+    .body {
+      margin-top: 7rem;
+      padding: -0.5rem;
 
-			.ext-links {
-				width: 60%;
-				margin-top: 4rem;
+      .intro-text {
+        width: 70%;
+        font-size: 1.5rem;
+      }
 
-				.link {
-					font-weight: 600; // semibold
-					font-size: 1.5rem;
-					margin-right: 1rem;
+      .ext-links {
+        width: 60%;
+        margin-top: 4rem;
 
-					.desk {
-						display: inline;
-					}
-					.mobile {
-						display: none;
-					}
+        .link {
+          font-weight: 600; // semibold
+          font-size: 1.5rem;
+          margin-right: 1rem;
 
-					:global(span) {
-						margin-left: 1rem;
-					}
-				}
-			}
-		}
-	}
+          .desk {
+            display: inline;
+          }
+
+          .mobile {
+            display: none;
+          }
+
+          :global(span) {
+            margin-left: 1rem;
+          }
+        }
+      }
+    }
+  }
 </style>
