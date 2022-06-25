ecf4d4e69676429cbb49d5091b464474b2ecdcab
Alex <alexandre.olemans@gmail.com>
2022-06-25 21:20:09 +0200

    fixes the logo component

--- src/components/icons/Logo.svelte
+++ src/components/icons/Logo.svelte
@@ -4,12 +4,12 @@
   export let size = '2';
   export let color = 'white';
   export let icon_color = color;
-  export let isClickable = true;
+  export let isClickable = "true";
 
   let size_number = size.replace(/[^\d.-]/g, ''); // it removes everything that is not a number.
 </script>
 
-{#if isClickable}
+{#if isClickable == "true"}
 <a class="container" href="/">
   <Icon style="margin-right:{size_number / 2}rem;" size="{size_number}rem" stroke={icon_color} />
   <span style="font-size: {size_number}rem; color: {color}; font-weight: 600;">Puroto</span>
