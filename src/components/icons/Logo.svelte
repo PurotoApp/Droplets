65753680ff336714791077a5866f78e566bc71a7
Alex <alexandre.olemans@gmail.com>
2022-06-25 20:42:26 +0200

    Add the logo component

---
+++ src/components/icons/Logo.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Icon from '@/icons/Icon.svelte';
+
+  export let size = '2';
+  export let color = 'white';
+  export let icon_color = color;
+
+  let size_number = size.replace(/[^\d.-]/g, ''); // it removes everything that is not a number.
+</script>
+
+<div class="container">
+    <Icon style="margin-right:{size_number / 2}rem;" size="{size_number}rem" stroke={icon_color} />
+    <span style="font-size: {size_number}rem; color: {color}; font-weight: 600;">Puroto</span>
+</div>
+
+<style lang="scss">
+  .container {
+    display: flex;
+    align-items: center;
+    user-select: none;
+    padding: 0.5rem;
+  }
+</style>
