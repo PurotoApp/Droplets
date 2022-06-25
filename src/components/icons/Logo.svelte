8e71c9583111d25a4e8b87c707d40199c0680a54
Alex <alexandre.olemans@gmail.com>
2022-06-25 21:08:31 +0200

    Add the ability to make the logo clickable

--- src/components/icons/Logo.svelte
+++ src/components/icons/Logo.svelte
@@ -4,14 +4,22 @@
   export let size = '2';
   export let color = 'white';
   export let icon_color = color;
+  export let isClickable = true;
 
   let size_number = size.replace(/[^\d.-]/g, ''); // it removes everything that is not a number.
 </script>
 
+{#if isClickable}
+<a class="container" href="/">
+  <Icon style="margin-right:{size_number / 2}rem;" size="{size_number}rem" stroke={icon_color} />
+  <span style="font-size: {size_number}rem; color: {color}; font-weight: 600;">Puroto</span>
+</a>
+{:else}
 <div class="container">
     <Icon style="margin-right:{size_number / 2}rem;" size="{size_number}rem" stroke={icon_color} />
     <span style="font-size: {size_number}rem; color: {color}; font-weight: 600;">Puroto</span>
 </div>
+{/if}
 
 <style lang="scss">
   .container {
