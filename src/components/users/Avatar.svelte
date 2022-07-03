723febff1e5a21a2c17bc02b5ac382e415e2f763
Alex <alexandre.olemans@gmail.com>
2022-07-03 17:52:44 +0200

    make the avatar clickable

--- src/components/users/Avatar.svelte
+++ src/components/users/Avatar.svelte
@@ -7,7 +7,7 @@
 
 {#if user != null}
   {#if typeof user.avatar == "string"}
-  <div class="avatar" style="width:{size};height:{size};border-radius:0.5rem;background:url({user.avatar});background-size:cover;background-position:center;background-repeat:no-repeat;margin-left:auto;margin-right:0;cursor:pointer;" />
+  <div class="avatar" style="width:{size};height:{size};border-radius:0.5rem;background:url({user.avatar});background-size:cover;background-position:center;background-repeat:no-repeat;margin-left:auto;margin-right:0;cursor:pointer;" on:click={()=>{goto(`/user/${user.username}`)}}/>
   {:else}
   <div class="avatar" style="width:{size};height:{size};border-radius:0.5rem;background-color:#26232C;margin-left:auto;margin-right:0;" />
   {/if}
