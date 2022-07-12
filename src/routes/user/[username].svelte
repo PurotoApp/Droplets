37836a334a1f1714a944649e87683331fe0f6147
Alex <alexandre.olemans@gmail.com>
2022-07-12 18:10:17 +0200

    account page

---
+++ src/routes/user/[username].svelte
@@ -0,0 +1,32 @@
+<script>
+import { faker } from 'https://cdn.skypack.dev/@faker-js/faker';
+
+  import { tick } from 'svelte';
+  import { page } from '$app/stores';
+
+  console.log($page.params.username);
+
+  let user = {
+    "id": null,
+    "name": null,
+    "username": null,
+    "avatar": null,
+    "banner": null,
+    "hasNotifications": false
+  }
+
+  $: {
+    renderAvatar($page.params.username);
+  }
+
+  async function renderAvatar(routeId) {
+    await tick();
+    let profilePicture = document.getElementById("avatar");
+    profilePicture.style.backgroundImage = `url(${faker.image.avatar()})`;
+  }
+
+</script>
+
+<svelte:head>
+  <title>Puroto - {$page.params.username}</title>
+</svelte:head>
