07fe0c40706377f5f1de0d6de33da2840606493e
Alex <alexandre.olemans@gmail.com>
2022-07-14 13:01:18 +0200

    Update [username].svelte

--- src/routes/user/[username].svelte
+++ src/routes/user/[username].svelte
@@ -1,32 +1,35 @@
 <script>
-import { faker } from 'https://cdn.skypack.dev/@faker-js/faker';
+  // FAKERJS IS A VERY LARGE PACKAGE AND SHOULDN'T BE DEPLOYED TO PRODUCTION, ONLY FOR TESTING PURPOSES.
+  // IF THE /user/[username] PAGE TAKES TIME TO LOAD, IT'S A NORMAL BEHAVIOR BECAUSE OF THE SIZE OF FAKERJS.
+  // THIS HAS TO BE USED SINCE THE API ISN'T READY YET.
+  import { faker } from 'https://cdn.skypack.dev/@faker-js/faker';
 
   import { tick } from 'svelte';
   import { page } from '$app/stores';
 
-  console.log($page.params.username);
-
   let user = {
-    "id": null,
-    "name": null,
-    "username": null,
-    "avatar": null,
+    "id": faker.datatype.uuid(),
+    "name": faker.name.firstName(),
+    "username": $page.params.username,
+    "bio": `${faker.name.jobArea()}: ${faker.name.jobTitle()} / ${faker.name.gender()} / Age of ${faker.datatype.number({min: 13, max: 99})}`,
+    "avatar": faker.image.avatar(),
     "banner": null,
     "hasNotifications": false
   }
 
   $: {
-    renderAvatar($page.params.username);
+    renderAvatar();
   }
 
-  async function renderAvatar(routeId) {
+  async function renderAvatar() {
     await tick();
     let profilePicture = document.getElementById("avatar");
-    profilePicture.style.backgroundImage = `url(${faker.image.avatar()})`;
+    profilePicture.style.backgroundImage = `url(${user.avatar})`;
   }
 
 </script>
 
 <svelte:head>
   <title>Puroto - {$page.params.username}</title>
+  <meta name="description" content={user.bio} />
 </svelte:head>
